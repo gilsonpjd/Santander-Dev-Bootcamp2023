ee4cdadda10d224c34c64109ac9a3798c56e1e04
José Junior <gilson.pjd@gmail.com>
2023-10-20 23:30:30 +0100

    Commit inital

---
+++ src/main/java/me/dio/SantanderDevBootcamp2023Application.java
@@ -0,0 +1,13 @@
+package me.dio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SantanderDevBootcamp2023Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SantanderDevBootcamp2023Application.class, args);
+	}
+
+}
