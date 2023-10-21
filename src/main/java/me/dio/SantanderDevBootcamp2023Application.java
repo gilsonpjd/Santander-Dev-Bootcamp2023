8c7824f61a74357050fc2cfdda0912c425c74cb9
José Junior <gilson.pjd@gmail.com>
2023-10-21 14:35:04 +0100

    Project finished

--- src/main/java/me/dio/SantanderDevBootcamp2023Application.java
+++ src/main/java/me/dio/SantanderDevBootcamp2023Application.java
@@ -2,8 +2,12 @@ package me.dio;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@EntityScan("me.dio.domain.model")
+@ComponentScan("me.dio.domain.repository")
 public class SantanderDevBootcamp2023Application {
 
 	public static void main(String[] args) {
--- src/main/java/me/dio/domain/model/User.java
+++ src/main/java/me/dio/domain/model/User.java
@@ -1,6 +1,8 @@
 package me.dio.domain.model;
 
+
 import jakarta.persistence.*;
+
 import java.util.List;
     @Entity(name = "tb_user")
     public class User {
